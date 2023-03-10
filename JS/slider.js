b0d6545f14bee56c7785fa9b3d78997b33c7950a
Sergio-FilhoIF <sergiodasilva0201@gmail.com>
2023-03-10 13:12:09 -0300

    Adicionando icones

--- JS/slider.js
+++ JS/slider.js
@@ -1,67 +1,67 @@
-const buttons = document.querySelectorAll('.btn')
-const boxs = document.querySelectorAll('.slide')
+// const buttons = document.querySelectorAll('.btn')
+// const boxs = document.querySelectorAll('.slide')
 
-const maxLenght = boxs.length
+// const maxLenght = boxs.length
 
-let count = 1
-document.getElementById('radio1').checked = true
+// let count = 1
+// document.getElementById('radio1').checked = true
 
-let interval = setInterval(() =>{
-    nextImage()
-},5000)
+// let interval = setInterval(() =>{
+//     nextImage()
+// },5000)
 
 
-buttons.forEach((btn) =>{
-    btn.addEventListener('click', () =>{
-        const esquerdo = btn.classList.contains('esquerdo')
-        const direito = btn.classList.contains('direito')
+// buttons.forEach((btn) =>{
+//     btn.addEventListener('click', () =>{
+//         const esquerdo = btn.classList.contains('esquerdo')
+//         const direito = btn.classList.contains('direito')
 
-        if(esquerdo){
-            if(count <= 1){
-                count = maxLenght
-                document.getElementById('radio'+count).checked = true
-                clearInterval(interval)
-                interval = setInterval(() =>{
-                    nextImage()
-                },5000)
-            }else{
-                count = count - 1
-                document.getElementById('radio'+count).checked = true
-                clearInterval(interval)
-                interval = setInterval(() =>{
-                    nextImage()
-                },5000)
+//         if(esquerdo){
+//             if(count <= 1){
+//                 count = maxLenght
+//                 document.getElementById('radio'+count).checked = true
+//                 clearInterval(interval)
+//                 interval = setInterval(() =>{
+//                     nextImage()
+//                 },5000)
+//             }else{
+//                 count = count - 1
+//                 document.getElementById('radio'+count).checked = true
+//                 clearInterval(interval)
+//                 interval = setInterval(() =>{
+//                     nextImage()
+//                 },5000)
                 
                 
-            }
-        }
+//             }
+//         }
 
-        if(direito){
-            if(count >= maxLenght){
-                count = 1
-                document.getElementById('radio'+count).checked = true
-                clearInterval(interval)
-                interval = setInterval(() =>{
-                    nextImage()
-                },5000)
+//         if(direito){
+//             if(count >= maxLenght){
+//                 count = 1
+//                 document.getElementById('radio'+count).checked = true
+//                 clearInterval(interval)
+//                 interval = setInterval(() =>{
+//                     nextImage()
+//                 },5000)
 
-            }else{
-                count = count + 1
-                document.getElementById('radio'+count).checked = true
-                clearInterval(interval)
-                interval = setInterval(() =>{
-                    nextImage()
-                },5000)
+//             }else{
+//                 count = count + 1
+//                 document.getElementById('radio'+count).checked = true
+//                 clearInterval(interval)
+//                 interval = setInterval(() =>{
+//                     nextImage()
+//                 },5000)
 
-            }
-        }
-    })
-})
-const nextImage = () =>{
-    count++
-    if(count>maxLenght){
-        count = 1
-    }
+//             }
+//         }
+//     })
+// })
+// const nextImage = () =>{
+//     count++
+//     if(count>maxLenght){
+//         count = 1
+//     }
 
-    document.getElementById('radio'+count).checked = true
-}
+//     document.getElementById('radio'+count).checked = true
+// }
